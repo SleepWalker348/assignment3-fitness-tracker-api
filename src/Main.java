d0c0896e7827ca7860fb5289afb9e579a5038c6b
SleepWalker348 <togtr456@gmail.com>
2026-02-02 01:02:00 +0500

    init: migrate project from milestone1 to milestone2 structure

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,43 @@
-
+import controllers.ExerciseController;
+import controllers.WorkoutController;
 import data.DatabaseConnection;
-import exception.DatabaseOperationException;
-import exception.DuplicateResourceException;
-import exception.InvalidInputException;
-import exception.ResourceNotFoundException;
-import model.CardioExercise;
-import model.CardioWorkout;
-import model.Exercise;
-import model.StrengthExercise;
-import model.StrengthWorkout;
-import model.Workout;
-import repository.ExerciseRepository;
-import repository.WorkoutRepository;
-import service.ExerciseService;
-import service.WorkoutService;
+import exceptions.DatabaseOperationException;
+import exceptions.DuplicateResourceException;
+import exceptions.InvalidInputException;
+import exceptions.ResourceNotFoundException;
+import models.CardioExercise;
+import models.CardioWorkout;
+import models.Exercise;
+import models.StrengthExercise;
+import models.StrengthWorkout;
+import models.Workout;
+import models.interfaces.Validatable;
+import repositories.ExerciseRepository;
+import repositories.WorkoutRepository;
+import services.ExerciseService;
+import services.WorkoutService;
+import services.interfaces.IExerciseService;
+import services.interfaces.IWorkoutService;
+import utils.ReflectionUtils;
+import utils.SortingUtils;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         DatabaseConnection db = new DatabaseConnection();
         WorkoutRepository workoutRepo = new WorkoutRepository(db);
         ExerciseRepository exerciseRepo = new ExerciseRepository(db);
-        WorkoutService workoutService = new WorkoutService(workoutRepo, exerciseRepo);
-        ExerciseService exerciseService = new ExerciseService(exerciseRepo, workoutService);
+        IWorkoutService workoutService = new WorkoutService(workoutRepo, exerciseRepo);
+        IExerciseService exerciseService = new ExerciseService(exerciseRepo, workoutService);
+        WorkoutController workoutController = new WorkoutController(workoutService);
+        ExerciseController exerciseController = new ExerciseController(exerciseService);
 
         try {
             StrengthWorkout newStrengthWorkout = new StrengthWorkout("Upper Body", "Intermediate",
                     LocalDate.of(2026, 1, 20), 60, "Chest and Back");
-            workoutService.createWorkout(newStrengthWorkout);
-            System.out.println("Created new strength workout");
+            System.out.println(workoutController.create(newStrengthWorkout));
 
             List<Workout> workouts = workoutService.getAllWorkouts();
             StrengthWorkout createdStrengthWorkout = null;
@@ -45,26 +52,22 @@ public class Main {
             }
             int workoutId = createdStrengthWorkout.getWorkoutId();
 
-
             StrengthExercise benchPress = new StrengthExercise(workoutId, "Bench Press", 4, 8, 100.0);
-            exerciseService.createExercise(benchPress);
-            System.out.println("Added Bench Press exercise");
+            System.out.println(exerciseController.create(benchPress));
 
             StrengthExercise pullUps = new StrengthExercise(workoutId, "Pull-Ups", 3, 10, 0.0);
-            exerciseService.createExercise(pullUps);
-            System.out.println("Added Pull-Ups exercise");
+            System.out.println(exerciseController.create(pullUps));
 
 
 
-            CardioWorkout newCardioWorkout = new CardioWorkout( "Jog", "Beginner",
-                    LocalDate.of(2026, 1, 15), 30, 140);
-            workoutService.createWorkout(newCardioWorkout);
-            System.out.println("Created new cardio workout");
+            CardioWorkout newCardioWorkout = new CardioWorkout("Cardio Session", "Beginner",
+                    LocalDate.of(2026, 1, 21), 45, 140);
+            System.out.println(workoutController.create(newCardioWorkout));
 
             workouts = workoutService.getAllWorkouts();
             CardioWorkout createdCardioWorkout = null;
             for (Workout w : workouts) {
-                if (w.getName().equals("Jog")) {
+                if (w.getName().equals("Cardio Session")) {
                     createdCardioWorkout = (CardioWorkout) w;
                     break;
                 }
@@ -74,64 +77,82 @@ public class Main {
             }
             int cardioWorkoutId = createdCardioWorkout.getWorkoutId();
 
-
-            CardioExercise jog = new CardioExercise(cardioWorkoutId, "Jogging", 5.0, 30);
-            exerciseService.createExercise(jog);
-            System.out.println("Added Jogging exercise");
+            CardioExercise running = new CardioExercise(cardioWorkoutId, "Running", 5.0, 30);
+            System.out.println(exerciseController.create(running));
 
 
 
             System.out.println();
-            System.out.println("All Workouts:");
+            System.out.println(" - - - ");
+            System.out.println();
+            System.out.println(workoutController.getAll());
+
+
+
+            System.out.println();
+            System.out.println(" - - - ");
+            System.out.println();
             workouts = workoutService.getAllWorkouts();
+            SortingUtils.sortWorkoutsByExerciseCount(workouts, true);
+            System.out.println("Sorted workouts by exercise count:");
             for (Workout w : workouts) {
-                System.out.println(w.toString());
-                System.out.println("Calories burned: " + w.calories());
-                System.out.println("Recovery time: " + w.getRecoveryTime() + " hours");
-                System.out.println("Days since workout: " + w.numberOfDays());
-                System.out.println("Exercises");
-                for (Exercise e : w.getExercises()) {
-                    System.out.println(" - Exercise: " + e.display());
-                }
+                System.out.println(w.getName() + " - Exercises: " + w.getExercises().size());
             }
 
 
 
+            System.out.println();
+            System.out.println(" - - - ");
+            System.out.println();
+            ReflectionUtils.printClassInfo(StrengthWorkout.class);
+
+
+
+            System.out.println();
+            System.out.println(" - - - ");
+            System.out.println();
+            boolean isValid = Validatable.isValid(newStrengthWorkout);
+            System.out.println("Is valid: " + isValid);
+
+
+
+            System.out.println();
+            System.out.println(" - - - ");
+            System.out.println();
             StrengthWorkout updatedWorkout = new StrengthWorkout("Updated Upper Body", "Advanced",
                     LocalDate.of(2026, 1, 22), 75, "Chest, Back, Shoulders");
-            workoutService.updateWorkout(workoutId, updatedWorkout);
-            System.out.println("Updated workout with ID " + workoutId);
-
-
+            System.out.println(workoutController.update(workoutId, updatedWorkout));
 
             List<Exercise> exercises = exerciseService.getAllExercises();
             if (!exercises.isEmpty()) {
-                int exerciseIdToDelete = exercises.getFirst().getExerciseId();
-                exerciseService.deleteExercise(exerciseIdToDelete);
-                System.out.println("Deleted exercise with ID " + exerciseIdToDelete);
+                int exerciseIdToDelete = exercises.get(0).getExerciseId();
+                System.out.println(exerciseController.delete(exerciseIdToDelete));
             }
 
-            workoutService.deleteWorkout(cardioWorkoutId);
-            System.out.println("Deleted cardio workout with ID " + cardioWorkoutId);
+            System.out.println(workoutController.delete(cardioWorkoutId));
 
 
+
+            System.out.println();
+            System.out.println(" - - - ");
+            System.out.println();
 
             try {
                 StrengthWorkout invalidWorkout = new StrengthWorkout("", "Beginner",
                         LocalDate.now(), 3, "Legs");
-                workoutService.createWorkout(invalidWorkout);
+                System.out.println(workoutController.create(invalidWorkout));
             } catch (InvalidInputException e) {
                 System.out.println("Exception: " + e.getMessage());
             }
 
             StrengthWorkout sw = new StrengthWorkout("Leg day", "Beginner",
                     LocalDate.of(2025, 12, 19), 89, "Legs");
-            workoutService.createWorkout(sw);
+            System.out.println(workoutController.create(sw));
 
             try {
                 StrengthWorkout duplicate = new StrengthWorkout("Leg Day", "Advanced",
                         LocalDate.of(2026, 1, 20), 75, "Arms");
-                workoutService.createWorkout(duplicate);
+                System.out.println(workoutController.create(duplicate));
             } catch (DuplicateResourceException e) {
                 System.out.println("Exception: " + e.getMessage());
             }
@@ -145,7 +166,8 @@ public class Main {
         } catch (InvalidInputException | ResourceNotFoundException | DatabaseOperationException e) {
             System.err.println("Error: " + e.getMessage());
         } catch (Exception e) {
-            System.err.println("Another error: " + e.getMessage());
+            System.out.println("Unexpected error: " + e.getClass().getSimpleName() + " - " + e.getMessage());
         }
     }
 }
+
